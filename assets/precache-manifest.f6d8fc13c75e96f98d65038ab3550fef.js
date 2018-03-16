304b2cbb4a26e517ade138d96212a269f659af6c
disjfa <disjfa@disjfa.nl>
2018-03-16 22:05:20 +0000

    Updates

---
+++ assets/precache-manifest.f6d8fc13c75e96f98d65038ab3550fef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fluent-bootstrap/assets/images/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fluent-bootstrap/assets/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fluent-bootstrap/assets/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fluent-bootstrap/assets/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fluent-bootstrap/assets/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "5f14f95aa6ed4ef12f76",
+    "url": "/fluent-bootstrap/assets/fluent-bootstrap.js"
+  },
+  {
+    "revision": "5f14f95aa6ed4ef12f76",
+    "url": "/fluent-bootstrap/assets/fluent-bootstrap.css"
+  }
+];
